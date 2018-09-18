c1663a1199324a900637bf62b479c6b775b939de
iiaii <jhun9409@naver.com>
2018-09-18 14:36:03 +0900

    test.ts 오류수정 async/await 추가

--- iiaii/test.ts
+++ iiaii/test.ts
@@ -3,18 +3,10 @@ const assert = chai.assert;
 const getMailList = require('./build/getMailList');
 const id: string = process.argv[5];
 const pwd: string = process.argv[6];
-const pageLoad = getMailList.pageLoad;
-const login = getMailList.login;
-const extractMailList = getMailList.extractMailList;
 const main = getMailList.main;
 // login(id, pwd) : link  
 // extractMailList(link) : list
 
-// 로그인 성공 확인
-const linkChk = (v): boolean => {
-    const re = new RegExp('(http|https|ftp|telnet|news|irc)://mail.google.com/([-/.a-zA-Z0-9_~#%$?&=:200-377()]+)', 'i');
-    return re.test(v);
-};
 // getMailList 모듈 결과 확인
 const resultChk = (value, text) => {
     value.forEach((element) => {
@@ -23,8 +15,8 @@ const resultChk = (value, text) => {
 };
 
 describe('# 메일 정보 추출 테스트', () => {
-    it('메일 리스트(제목,보낸사람) 리턴?', () => {
-        const result: any = main(id, pwd);
+    it('메일 리스트(제목,보낸사람) 리턴?', async () => {
+        const result: any = await main(id, pwd);
 
         // 테스트 성공
         if (result.status === '200') {
@@ -36,5 +28,5 @@ describe('# 메일 정보 추출 테스트', () => {
         else if(result.status === '400') {  
             
         }*/
-    }).timeout(10000);
+    }).timeout(20000);
 });
