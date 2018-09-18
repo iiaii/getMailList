60a86b62d1ce0e5a95505e1fb0c16d569e54df60
iiaii <jhun9409@naver.com>
2018-09-18 14:25:52 +0900

    메일정보 추출 후 불필요한 코드 삭제- test.ts

--- iiaii/test.ts
+++ iiaii/test.ts
@@ -23,15 +23,17 @@ const resultChk = (value, text) => {
 };
 
 describe('# 메일 정보 추출 테스트', () => {
-    it('메일 리스트(제목,보낸사람) 리턴?', async () => {
-        const result: any = await main(id, pwd);
+    it('메일 리스트(제목,보낸사람) 리턴?', () => {
+        const result: any = main(id, pwd);
 
         // 테스트 성공
         if (result.status === '200') {
             assert.isObject('', '형식 확인');
             resultChk(result.name, '보낸이름 -> 유효값인지 확인');
             resultChk(result.title, '메일제목 -> 유효값인지 확인');
-        } /*else {
+        } /*
+        // 테스트 실패
+        else if(result.status === '400') {  
             
         }*/
     }).timeout(10000);
